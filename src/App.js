8fd04b4e52fd4f5db77ae854acccd95c4cecda2d
NayWin23 <letmecode123456@gmail.com>
2020-06-10 21:55:32 +0630

    responsive

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ export default class App extends Component {
     return (
        <div className="container">
          <div className="row">
-           <div className="col-10 mx-auto col-md-12 mt-4">
+           <div className="col-10 mx-auto col-md-8 mt-4">
              <h3 className="text-capitalize text-center">ToDo App</h3>
              <TodoInput
               item={this.state.item} 
